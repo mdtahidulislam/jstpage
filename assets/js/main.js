5d15ec531270de7c646aeaceadbd435b3c2d7d06
mdtahidulislam <tahidul.developer@gmail.com>
2023-01-11 11:59:43 +0600

    fix for 100vh

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,5 +167,13 @@
         // interSectObserver('.js-img-overlay');
 
         revealSpltTextAnim('.mobile-menu li a');
+
+        // fix 100vh for ios
+        const appHeight = () => {
+            const doc = document.documentElement
+            doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+        }
+        window.addEventListener('resize', appHeight)
+        appHeight();
     });
 }(jQuery));
